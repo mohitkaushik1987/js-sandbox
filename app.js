b5e87f8ea820ed7d0abb4530823b01ee72128ffc
mohitkaushik1987 <mohitkaushik0711@gmail.com>
2018-11-20 21:23:27 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,67 @@
+//alert('You Sure?');
+// console.log('Hello World!');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello!';
+// console.log(greeting);
+// console.log([1,2,3,45,56]);
+// console.log({a:2,b:4});
+// console.table({a:2,b:6});
+// console.error('This is some error');
+// console.warn('This is a warning');
+// console.clear();
+
+// console.time('time1');
+//     console.table({a:2,b:6});
+//     console.table({a:2,b:6});
+//     console.table({a:2,b:6});
+//     console.table({a:2,b:6});
+//     console.table({a:2,b:6});
+// console.timeEnd('time1');
+// console.clear();
+
+// var name = 'John Doe'
+// console.log(name)
+// name = 'Steve Smith'
+// console.log(name)
+
+// var greeting
+// console.log(greeting)
+// greeting = 'hello'
+// console.log(greeting)
+// // letters, numbers, _, $
+// // Can't start with a number
+
+// var firstName = 'Don';
+// var first_name = 'Sarah';
+// var FirstName = 'Tom';
+// console.log(firstName,first_name,FirstName);
+
+//let
+let name = 'John Doe'
+console.log(name)
+name = 'Steve Smith'
+console.log(name)
+
+//CONST
+const lastName = 'proxy';
+console.log(lastName);
+//lastName = 'troxy';
+//console.log(lastName);
+
+const person = {
+    name: 'John',
+    age: 31
+}
+
+person.name = 'Sarah';
+person.age = 32;
+//console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+numbers.push(7);
+numbers.pop();
+numbers.pop();
+//numbers = [1,2,1,1,2,1,21]; //can't reassign const
+console.log(numbers);
